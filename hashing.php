eca24cc1a1537a40e2ed5e551b680679a9558988
Lisa Halas <Lisa Halas>
2017-05-15 17:48:43 -0400

    Added comments

--- hashing.php
+++ hashing.php
@@ -14,11 +14,12 @@
 <?php
 echo "<h2>Current Hash Algorithms</h2>";
 
-
+// When the submit button is pushed
 if(isset($_POST["sub"])) {
   $pass = $_POST["text"];
-
+// If the variable is not blank
   if ($pass != "") {
+    // hash_algos() returns current supported hash algorithms
     for ($i=0; $i < count(hash_algos()); $i++) {
       echo "<b>". (hash_algos()[$i]) . ":</b> " . hash(hash_algos()[$i],$pass) . "<br />";
     }
